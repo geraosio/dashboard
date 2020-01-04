c5115c98249acd871d3e28a39e8cd66b2ffe3724
geraosio <geraosio@gmail.com>
2020-01-04 18:58:05 +0100

    Add global styles

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,32 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
+@import "@/styles/colors.scss";
+@import "@/styles/typography.scss";
+
+body {
+  background-color: $dark-blue;
+}
+
+h1 {
+  @include heading-1;
+}
+
+p {
+  @include large-text-bold($purple);
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: $system-font-family;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $white;
 }
 
 #nav {
@@ -25,7 +40,7 @@
     color: #2c3e50;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: $white;
     }
   }
 }
