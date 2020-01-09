f21872168f64f85caa9cbac50d65251e5d42102b
geraosio <geraosio@gmail.com>
2020-01-09 07:07:34 +0100

    Change dark mode option to the whole app

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,38 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
+const state = {
+  isDarkMode: true
+}
+
+const getters = {
+  isDarkMode(state) {
+    return state.isDarkMode
   }
+}
+
+const mutations = {
+  toggleDarkMode(state) {
+    if (state.isDarkMode === true) {
+      state.isDarkMode = false
+      document.body.style.backgroundColor = '#f0f3f5'
+    } else {
+      state.isDarkMode = true
+      document.body.style.backgroundColor = '#212c4f'
+    }
+  }
+}
+
+const actions = {
+  triggerDarkMode(context) {
+    context.commit('toggleDarkMode')
+  }
+}
+
+export default new Vuex.Store({
+  state,
+  getters,
+  mutations,
+  actions,
+  modules: {}
 })
