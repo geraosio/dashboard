c7cc58899593a986292f515b27e15c40da7eb4d0
geraosio <geraosio@gmail.com>
2020-01-13 07:15:37 +0100

    Add GoTrue JS to customize Netlify's sign in UI

--- src/main.js
+++ src/main.js
@@ -3,11 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import * as netlifyIdentityWidget from 'netlify-identity-widget'
+import GoTrue from 'gotrue-js'
 
 Vue.config.productionTip = false
 
+//
+// Netlify Config
+//
 // Initialize Netlify Identity
 netlifyIdentityWidget.init()
+// Initialize GoTrue JS for Netlify
+export const auth = new GoTrue({
+  APIUrl: 'https://geraosio-dashboard.netlify.com/.netlify/identity',
+  setCookie: true // Remember that the user is logged in
+})
 
 new Vue({
   router,
